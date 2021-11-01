45de710c0740aefcf19a5803b05d56b8cbc30de7
Karine Mendes <karinemendessantos@hotmail.com>
2021-11-01 18:18:14 -0300

    Feat: add back-end server for the project

---
+++ server/routes.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var routes = express.Router();
+var data = [];
+
+routes.get('/', function (req, res) {
+    res.json(data);
+});
+
+routes.post('/', function (req, res) {
+    data.push({
+        image: req.body.image,
+        brandModel: req.body.brandModel,
+        year: req.body.year,
+        plate: req.body.plate,
+        color: req.body.color
+    });
+    res.json('Carro cadastrado com êxito')
+});
+
+module.exports = routes;
